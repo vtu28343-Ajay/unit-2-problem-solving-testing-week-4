96a2f7cc2ce378964e92f2eac9be4d4105c76147
vtu28343-Ajay <vtu28343@veltech.edu.in>
2026-09-10 12:20:28 +0530

    Time: 72 ms (53.28%), Space: 111.2 MB (62.91%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <unordered_set>
+
+class Solution {
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+        return false;
+    }
+};
